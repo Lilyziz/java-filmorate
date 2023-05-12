cfa41ecaa26319d443c34cd44941a6632c8d6c11
V|ka <qianna@yandex.ru>
2023-05-13 01:09:15 +0300

    ТЗ 10 ver 1.6

--- src/main/java/ru/yandex/practicum/filmorate/exception/InternalServerError.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/InternalServerError.java
@@ -2,9 +2,11 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class InternalServerError extends RuntimeException {
     private final String parameter;
+
     public InternalServerError(String parameter) {
         this.parameter = parameter;
     }
+
     public String getParameter() {
         return parameter;
     }
--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -2,9 +2,11 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class ValidationException extends RuntimeException {
     private final String parameter;
+
     public ValidationException(String parameter) {
         this.parameter = parameter;
     }
+    
     public String getParameter() {
         return parameter;
     }
