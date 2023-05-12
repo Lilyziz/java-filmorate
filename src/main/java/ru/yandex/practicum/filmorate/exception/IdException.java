f9a684d2a51f0a85535d4d2e83a04dd6f090ab31
V|ka <qianna@yandex.ru>
2023-05-13 01:08:12 +0300

    ТЗ 10 ver 1.5

--- src/main/java/ru/yandex/practicum/filmorate/exception/IdException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/IdException.java
@@ -2,9 +2,11 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class IdException extends RuntimeException {
     private final String parameter;
+
     public IdException(String parameter) {
         this.parameter = parameter;
     }
+
     public String getParameter() {
         return parameter;
     }
