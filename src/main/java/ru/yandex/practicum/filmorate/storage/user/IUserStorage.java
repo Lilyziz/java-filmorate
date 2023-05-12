b343a05c8715c5ac2ea9e80559a705560aaf410b
V|ka <qianna@yandex.ru>
2023-05-13 01:16:48 +0300

    ТЗ 10 ver 1.10

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/IUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/IUserStorage.java
@@ -6,10 +6,15 @@ import java.util.Collection;
 
 public interface IUserStorage {
     Collection<User> getAllUsers();
+
     User createUser(User user);
+
     User updateUser(User user);
+
     //void update(User updatingUser, User user);
     void delete(Long id);
+
     User getUserById(Long id);
+    
     boolean contains(long id);
 }
